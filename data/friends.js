7872a06ac6d1043db83a9f6e45f58dcb9b0b2f89
aanchal <aanchal.g.gupta@gmail.com>
2016-10-31 16:01:49 -0400

    Files uploaded

---
+++ data/friends.js
@@ -0,0 +1,30 @@
+
+var friendsArray = [
+  {
+    FriendName: 'Reema',
+    FriendPhotolink: '',
+    Answers: '1,1,1,1,1,1,,1,1,1,1'
+
+  },
+
+{
+    FriendName: 'Reema',
+    FriendPhotolink: '',
+    Answers: '2,2,2,2,2,2,2,2,2,2'
+
+  },
+{
+    FriendName: 'Reema',
+    FriendPhotolink: '',
+    Answers: '3,3,3,3,3,3,3,3,3,3,'
+
+  },
+{
+    FriendName: 'Reema',
+    FriendPhotolink: '',
+    Answers: '4,4,4,4,4,4,4,4,4,4'
+
+  }
+
+];
+module.exports = friendsArray;
---
+++ routing/api-routes.js
@@ -0,0 +1,41 @@
+
+// We need to include the path package to get the correct file path for our html
+var path = require('path');
+
+var friendsData     = require('../data/friends.js');
+
+module.eports = function(app){
+
+
+app.get('/public/survey.html', function (req, res) {
+    console.log('root access requested');
+    res.sendFile(path.join(__dirname, '/../public/survey.html'));
+  });
+
+
+  app.get('/friendsArray', function(req, res) {
+    console.log('friendsArray requested');
+    res.json(friendsArray);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
